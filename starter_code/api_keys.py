2908bf8c1328f8e7bea168029d535843991b2ea6
Jose Salinas <152557745+JoseSali@users.noreply.github.com>
2024-06-08 22:03:44 -0500

    Update api_keys.py

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "6cfe69cbd0249b457b5873157c2e26e8"
+weather_api_key = "your api key"
 
 # Geoapify API Key
-geoapify_key = "882bc415122c4b47bc797dccd696ce27"
+geoapify_key = "your api key"
